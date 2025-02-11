b1b6deee8ea7ef80487761ce8017c87dae6d5258
HunorAbraham <hunor335@gmail.com>
2025-02-11 11:54:50 +0100

    what

--- script.js
+++ script.js
@@ -75,10 +75,23 @@ class GRASS{
 new GRASS({x:600,y:900}, 200, [{rn: 0, ig: 0, tl: 5, rl: 0, gr: 2}, {rn: 0, ig: 50, tl: 10, rl: 0, gr: 1},{rn: 100, ig: 50, tl: 20, rl: 2, gr: 0}]);
 
 //return the magnitude of a 2d vector
-function vec2Mag(v){return Math.sqrt(v.x**2 + v.y**2)};
-function touch(any){
-    console.log(`touched: ${(any instanceof GRASS)?"grass":"not grass"}`);
+function vec2Mag(v){return Math.sqrt(v.x**2 + v.y**2);}
+//funny
+function touch(any){console.log(`touched: ${(any instanceof GRASS)?"grass":"not grass"}`);}
+//returns -1; 1 or 0
+//w is a (+/-) rotation from v, eg: (1,0) is a positive rotation, PI/2, from (0,1)
+//if angle between w and v is 0, 0 is returned
+function handedness(v,w){return Math.sign(v.x*w.y - w.x*v.y);}
+//returns a normal vector with anlge (Math.atan2) = ang
+function vec2FromAng(ang){return {x:Math.cos(ang), y:Math.sin(ang)};}
+function debugRenderVec2(v, point){
+    ctx.beginPath();
+    ctx.moveTo(point.x,point.y);
+    ctx.lineTo(point.x +v.x*100, point.y + v.y*100);
+    ctx.closePath();
+    ctx.stroke();
 }
+
 
 class CREATURE_STATE{
     constructor(nut){
@@ -101,7 +114,7 @@ class CREATURE_GENE{
             Math.random()*100 + 40,
             Math.random()*360,
             Math.random()*20+10,
-            Math.random()*20+10,
+            Math.random()*50+100,
         );
     };
     mutate(){
@@ -113,25 +126,41 @@ class CREATURE_GENE{
 class CREATURE{
     constructor(pos, genes, nutriens){
         this.p = pos; //position
-        this.v = {x:0, y:0}; //velocity
+        this.v = {x:1, y:1}; //velocity
         this.gs = genes; //genes
         this.st = new CREATURE_STATE(nutriens); //state of the creature
+        CREATURE.all.push(this);
+        this.dg = 0; //desired angle to head towards
+        this.texp = 1; //turn expirity, at most turn in one direction for 1 second then change direction
     }
+    static all = [];
     render(){
         ctx.beginPath();
         ctx.fillStyle = `hsl(${this.gs.col}, ${this.st.n}%, 50%)`;
         ctx.arc(this.p.x, this.p.y, this.gs.size, 0, Math.PI*2, false);
         ctx.closePath();
         ctx.fill();
+        debugRenderVec2(this.v,this.p);
     }
     update(){
-        this.st.n -= vec2Mag(this.v)*this.gs.size + this.gs.vis;
+        this.st.n -= (vec2Mag(this.v)/100*this.gs.size + this.gs.vis) * planc *0.1;
+        if(this.st.n <= 0) {this.die(); return;}
+        this.p.x += this.v.x *planc;
+        this.p.y += this.v.y *planc;
+
         switch(this.st.g){
             case "wander":
                 //make better, now runs forward on average -> BAD!
                 let ang = Math.atan2(this.v.y, this.v.x);
-                let newAng = ang + (Math.random()-0.5)*2*10; //here
-                this.v = {x: Math.cos(newAng)*this.gs.spd, y: Math.cos(newAng)*this.gs.spd}; //always going max speed right now
+                this.texp -= planc;
+                if(Math.abs(this.dg - ang) < this.gs.spd/180*Math.PI*planc || this.texp <= 0){
+                    this.dg = Math.random()*Math.PI*2;
+                    this.texp = 1;
+                    break;
+                }
+                let newAng = ang + handedness(vec2FromAng(this.dg),this.v) * this.gs.spd/180*Math.PI*planc; //here
+                this.v = {x: Math.cos(newAng)*this.gs.spd, y: Math.sin(newAng)*this.gs.spd}; //always going max speed right now
+                //this.lookAround();
                 break;
             case "goto":
                 console.error("not implemented");
@@ -147,4 +176,19 @@ class CREATURE{
                 break;
         }
     }
+    lookAround(){
+        console.error("not implemented");
+    }
+    die(){
+        if(!CREATURE.all.includes(this)) return;
+        CREATURE.all.splice(CREATURE.all.indexOf(this),1);
+    }
 }
+new CREATURE({x:600,y:450},CREATURE_GENE.random(), 100);
+setInterval(()=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    CREATURE.all.forEach(c=>{
+        c.update();
+        c.render();
+    });
+},deltaTime);
